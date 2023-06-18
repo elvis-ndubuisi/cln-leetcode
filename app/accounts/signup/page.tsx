e9c1b5177e0ab06aaad97f4f066720f652aca309
elvis-ndubuisi <provictor.ie@gmail.com>
2023-06-18 18:48:53 +0100

    Updated: auth pages mockup

--- app/accounts/signup/page.tsx
+++ app/accounts/signup/page.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
+
 import Input from "@/components/client/Input";
 
-export default function AuthSignIn() {
+export default function AuthSignUp() {
   return (
     <section className="flex-1 flex items-center justify-center px-2">
       <section className="drop-shadow-sm bg-white rounded px-6 py-8 w-full max-w-sm flex flex-col items-center">
@@ -15,19 +16,20 @@ export default function AuthSignIn() {
           className="mb-4"
         />
         <form className="w-full flex flex-col gap-4">
-          <Input name="name" placeholder="placeholder" />
-          <Input name="name" placeholder="placeholder" />
+          <Input name="username" placeholder="Username" />
+          <Input name="password" placeholder="Password" />
+          <Input name="c-password" placeholder="Confirm password" />
+          <Input name="email" type="email" placeholder="Email address" />
           <button
             type="submit"
             className="font-light text-sm text-white bg-gradient-to-r from-gray-700 to-gray-600 px-3 py-2 rounded-sm hover:bg-gradient-to-l hover:from-gray-500 hover:to-gray-400"
           >
-            Sign In
+            Sign Up
           </button>
         </form>
 
         <section className="font-light text-sm text-gray-8 w-full my-5 flex items-center justify-between">
-          <Link href="forgot-password">Forgot Password?</Link>
-          <Link href="signup">Sign Up</Link>
+          Have an account? <Link href="signin">Sign In</Link>
         </section>
 
         <section className="flex flex-col items-center justify-center gap-2">
