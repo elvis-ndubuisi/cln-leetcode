6d6f35c0347bf4f669de9717e3b0ebb5c11efa6d
elvis-ndubuisi <provictor.ie@gmail.com>
2023-06-20 21:40:32 +0100

    Added: second section custom background

---
+++ components/server/HomeSectionBg.tsx
@@ -0,0 +1,6 @@
+const HomeSectionBg: React.FC = () => {
+  return (
+    <div className="absolute top-[960px] left-0 right-0  h-[calc(80%_-_760px)] -z-20 -skew-y-12 bg-blue-50" />
+  );
+};
+export default HomeSectionBg;
