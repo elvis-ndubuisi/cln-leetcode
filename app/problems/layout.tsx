8ae2321caa13a2c45c441bd534163bf0b084c141
elvis-ndubuisi <provictor.ie@gmail.com>
2023-06-20 20:36:44 +0100

    Modified: new page route for problems

---
+++ app/problems/layout.tsx
@@ -0,0 +1,26 @@
+// import { Inter } from 'next/font/google'
+import ActionSideBar from "@/components/client/ActionSideBar";
+
+// const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: "Tricky Problems - LeetCode",
+  description:
+    "Cloned copy - Web application that contains leetcode problems and video solutions",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      {/* <body className={inter.className}>{children}</body> */}
+      <body className="bg-dark-layer-2 relative">
+        {children}
+        <ActionSideBar />
+      </body>
+    </html>
+  );
+}
---
+++ app/problems/page.tsx
@@ -0,0 +1,14 @@
+export default function Problems() {
+  return (
+    <>
+      <header className="bg-dark-layer-1">
+        <section className="wrapper flex items-center justify-between">
+          <p>leetcode</p>
+          <p>so</p>
+        </section>
+      </header>
+      <main></main>
+      <footer></footer>
+    </>
+  );
+}
